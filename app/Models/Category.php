61915e485c6e4119663c742c8bc271f5bacaced4
nguyenhathanh2212 <thanhtdk2212@gmail.com>
2018-05-13 14:31:12 +0700

    seed

---
+++ app/Models/Category.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    //
+    protected $fillable = [
+        'parent_id',
+        'name'
+    ];
+
+    protected $appends = ['numberOfProduct'];
+
+    public function getActionNews()
+    {
+        return $this->hasMany(ActionNews::class);
+    }
+
+    public function getExchanges()
+    {
+        return $this->hasMany(Exchange::class);
+    }
+
+    public function getNews()
+    {
+        return $this->hasMany(News::class);
+    }
+
+    public function getProjects()
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    public function subCategories()
+    {
+        return $this->hasMany(Category::class, 'parent_id', 'id');
+    }
+
+    public function parentCategory()
+    {
+        return $this->belongsTo(Category::class, 'parent_id', 'id');
+    }
+
+    public function getParentId(){
+        return $this->attributes['parent_id'];
+    }
+
+    public function getName(){
+        return $this->attributes['name'];
+    }
+}
--- database/migrations/2018_05_05_133957_create_videos_table.php
+++ database/migrations/2018_05_05_133957_create_videos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateVideosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('videos', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('product_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps(); 
+            $table->timestamps();
         });
     }
 
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('videos');
     }
 }
