42d76427e27535e05c5a16b82b513c1bab44cbc4
DESKTOP-E573FU7\vuong <vuong14t1@gmail.com>
2018-06-17 17:49:47 +0700

    [UPDATE] update giao dien header admin

--- resources/views/templates/admin/header.blade.php
+++ resources/views/templates/admin/header.blade.php
@@ -40,18 +40,18 @@
                 <!-- logo for regular state and mobile devices -->
                 <span class="logo-lg"><b>Admin</b></span>
             </a>
-            <!-- Header Navbar
+            <!-- Header Navbar -->
             <nav class="navbar navbar-static-top" role="navigation">
-                Sidebar toggle button
+                <!-- Sidebar toggle button-->
                 <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
                     <span class="sr-only">Toggle navigation</span>
                 </a>
-                Navbar Right Menu
+                <!-- Navbar Right Menu -->
                 <div class="navbar-custom-menu">
                     <ul class="nav navbar-nav">
-                        Messages: style can be found in dropdown.less
-                        <li class="dropdown messages-menu">
-                            Menu toggle button
+                        <!-- Messages: style can be found in dropdown.less-->
+                        {{-- <li class="dropdown messages-menu">
+                            <!-- Menu toggle button -->
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <i class="fa fa-envelope-o"></i>
                                 <span class="label label-success">4</span>
@@ -59,35 +59,35 @@
                             <ul class="dropdown-menu">
                                 <li class="header">You have 4 messages</li>
                                 <li>
-                                    inner menu: contains the messages
+                                    <!-- inner menu: contains the messages -->
                                     <ul class="menu">
-                                        <li>start message
+                                        <li><!-- start message -->
                                             <a href="#">
                                                 <div class="pull-left">
-                                                    User Image
+                                                    <!-- User Image -->
                                                     <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                                                 </div>
-                                                Message title and timestamp
+                                                <!-- Message title and timestamp -->
                                                 <h4>
                                                     Support Team
                                                     <small><i class="fa fa-clock-o"></i> 5 mins</small>
                                                 </h4>
-                                                The message
+                                                <!-- The message -->
                                                 <p>Why not buy a new awesome theme?</p>
                                             </a>
                                         </li>
-                                        end message
+                                        <!-- end message -->
                                     </ul>
-                                    /.menu
+                                    <!-- /.menu -->
                                 </li>
                                 <li class="footer"><a href="#">See All Messages</a></li>
                             </ul>
-                        </li>
-                        /.messages-menu
-            
-                        Notifications Menu
-                        <li class="dropdown notifications-menu">
-                            Menu toggle button
+                        </li> --}}
+                        <!-- /.messages-menu -->
+
+                        <!-- Notifications Menu -->
+                        {{-- <li class="dropdown notifications-menu">
+                            <!-- Menu toggle button -->
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <i class="fa fa-bell-o"></i>
                                 <span class="label label-warning">10</span>
@@ -95,22 +95,22 @@
                             <ul class="dropdown-menu">
                                 <li class="header">You have 10 notifications</li>
                                 <li>
-                                    Inner Menu: contains the notifications
+                                    <!-- Inner Menu: contains the notifications -->
                                     <ul class="menu">
-                                        <li>start notification
+                                        <li><!-- start notification -->
                                             <a href="#">
                                                 <i class="fa fa-users text-aqua"></i> 5 new members joined today
                                             </a>
                                         </li>
-                                        end notification
+                                        <!-- end notification -->
                                     </ul>
                                 </li>
                                 <li class="footer"><a href="#">View all</a></li>
                             </ul>
-                        </li>
-                        Tasks Menu
-                        <li class="dropdown tasks-menu">
-                            Menu Toggle Button
+                        </li> --}}
+                        <!-- Tasks Menu -->
+                        {{-- <li class="dropdown tasks-menu">
+                            <!-- Menu Toggle Button -->
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <i class="fa fa-flag-o"></i>
                                 <span class="label label-danger">9</span>
@@ -118,18 +118,18 @@
                             <ul class="dropdown-menu">
                                 <li class="header">You have 9 tasks</li>
                                 <li>
-                                    Inner menu: contains the tasks
+                                    <!-- Inner menu: contains the tasks -->
                                     <ul class="menu">
-                                        <li>Task item
+                                        <li><!-- Task item -->
                                             <a href="#">
-                                                Task title and progress text
+                                                <!-- Task title and progress text -->
                                                 <h3>
                                                     Design some buttons
                                                     <small class="pull-right">20%</small>
                                                 </h3>
-                                                The progress bar
+                                                <!-- The progress bar -->
                                                 <div class="progress xs">
-                                                    Change the css width attribute to simulate progress
+                                                    <!-- Change the css width attribute to simulate progress -->
                                                     <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
                                                     aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
                                                     <span class="sr-only">20% Complete</span>
@@ -137,42 +137,42 @@
                                             </div>
                                         </a>
                                     </li>
-                                    end task item
+                                    <!-- end task item -->
                                 </ul>
                             </li>
                             <li class="footer">
                                 <a href="#">View all tasks</a>
                             </li>
                         </ul>
-                    </li>
-                    User Account Menu
+                        </li> --}}
+                    <!-- User Account Menu -->
                     <li class="dropdown user user-menu">
-                        Menu Toggle Button
+                        <!-- Menu Toggle Button -->
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            The user image in the navbar
+                            <!-- The user image in the navbar-->
                             <img src="https://www.poolparadise.net/images/default_user.png" class="user-image" alt="User Image">
-                            hidden-xs hides the username on small devices so only the image appears.
+                            <!-- hidden-xs hides the username on small devices so only the image appears. -->
                             <span class="hidden-xs">{{ Auth::user()->fullname }}</span>
                         </a>
                         <ul class="dropdown-menu">
-                            The user image in the menu
+                            <!-- The user image in the menu -->
                             <li class="user-header">
                                 <img src="https://www.poolparadise.net/images/default_user.png" class="img-circle" alt="User Image">
                                 <p>
                                     {{ Auth::user()->fullname }}
-                                    <small>Member since Nov. 2012</small>
+                                    <small>Thời gian tạo: {{ Auth::user()->created_at }}</small>
                                 </p>
                             </li>
-                            Menu Footer
+                            <!-- Menu Footer-->
                             <li class="user-footer">
                                 <div class="pull-left">
-                                    <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                    <a href="{{ route('admin.contract.index') }}" class="btn btn-default btn-flat">Thông tin liên hệ</a>
                                 </div>
                                 <div class="pull-right">
                                     <a href="{{ route('logout') }}" class="btn btn-default btn-flat" 
                                     onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                     <span class="fa fa-power-off "></span> 
-                                    Sign out
+                                    Đăng xuất
                                 </a>
                                 {{ Form::open(['route' => 'logout', 'id' => 'logout-form']) }}
                                 {{ Form::close() }}
@@ -180,13 +180,13 @@
                         </li>
                     </ul>
                 </li>
-                Control Sidebar Toggle Button
+                <!-- Control Sidebar Toggle Button -->
                         {{-- <li>
                             <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
                         </li> --}}
                     </ul>
                 </div>
-            </nav> -->
+            </nav>
         </header>
         <!-- Left side column. contains the logo and sidebar -->
         <aside class="main-sidebar">
@@ -203,7 +203,7 @@
                 </div>
 
                 <!-- search form (Optional) -->
-                <form action="#" method="get" class="sidebar-form">
+               {{--  <form action="#" method="get" class="sidebar-form">
                     <div class="input-group">
                         <input type="text" name="q" class="form-control" placeholder="Search...">
                         <span class="input-group-btn">
@@ -211,23 +211,22 @@
                             </button>
                         </span>
                     </div>
-                </form>
+                </form> --}}
                 <!-- /.search form -->
 
                 <!-- Sidebar Menu -->
                 <ul class="sidebar-menu" data-widget="tree">
-                    <li class="header">Chức năng</li>
+                    <li class="header">Chức năng chính</li>
                     <!-- Optionally, you can add icons to the links -->
                     <li class="treeview">
                         <a href="#">
-                            <i class="fa fa-calendar"></i> <span>Liên lạc</span>
+                            <i class="fa fa-calendar"></i> <span>Liên hệ</span>
                             <span class="pull-right-container">
                                 <i class="fa fa-angle-left pull-right"></i>
                             </span>
                         </a>
-                        <ul class="treeview-menu">{{-- 
-                            <li><a href="{{ route('admin.contact.create') }}">Thêm</a></li> --}}
-                            <li><a href="{{ route('admin.contract.index') }}">Xem</a></li>
+                        <ul class="treeview-menu">
+                            <li><a href="{{ route('admin.contract.index') }}">Xem thông tin</a></li>
                         </ul>
                     </li>
                     <li class="treeview">
